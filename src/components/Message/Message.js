a674ae19eeb7c8fed5bb3c95eae04d28b3e1f6e5
Edu Cortada <cortadaedu@gmail.com>
2019-05-27 17:57:11 +0200

    New custom landbot

--- src/components/Message/Message.js
+++ src/components/Message/Message.js
@@ -4,14 +4,19 @@ import './style.css'
 
 const Message = ({ message }) => {
   const typeMessage = message.samurai < 0 ? 'is-bot' : 'is-human'
-
   return (
-    <article className={`message ${typeMessage}`}>
-      <p className="message-text">
-        {message.message}
-        <span className="message-text-time">{moment(message.timestamp).format('HH:mm')}</span>
-      </p>
-    </article>
+    message.type === 'text' ? (
+      <article className={`message ${typeMessage}`}>
+        <p className="message-text">
+          {message.message}
+          <span className="message-text-time">{moment(message.timestamp).format('HH:mm')}</span>
+        </p>
+      </article>
+    ) : message.type === 'image' && (
+      <figure>
+        <img className="message-img" src={message.url} alt={message.type} />
+      </figure>
+    )
   )
 }
 
--- src/lib/core.js
+++ src/lib/core.js
@@ -6,13 +6,13 @@ import 'firebase/database'
 
 const core = new Landbot.Core({
   firebase: firebase, // required
-  brandID: 12235, // required
-  channelToken: 'H-116929-N86QGRNNY6QCWEU3', // required
+  brandID: 65414, // required
+  channelToken: 'H-184336-XD5SP8AZUMKG44CG', // required
   welcomeUrl: 'https://welcome.landbot.io/', // recommended
   welcome: [{
     samurai: -1,
     type: 'text',
-    message: 'Hello! My name is Edu, can I help you?'
+    message: 'Hello! My name is Landbot.'
   }]
 })
 
